bf9baf4b991aeaae0ecc7566048a5452b63360e5
Joelstarl <joelasce@gmail.com>
2019-02-13 17:46:38 -0500

    Update servicio.js

--- servicio.js
+++ servicio.js
@@ -42,7 +42,7 @@ app.post("/api/tiempo", (req, res) => {
      
         let _body = JSON.parse(body);
 
-        console.log(_body['city']['coord']['lat']);
+        console.log(_body['city']['coord']);
         
         let forecast = _body['city']['coord'][0];
   
